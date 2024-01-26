e19fbcd6f077dc18232af1b3b9de538cf9ca7b13
Terran <terrankroft@gmail.com>
2024-01-26 19:34:53 +0800

    Initial Commit

---
+++ jpex/jpexApp.swift
@@ -0,0 +1,17 @@
+//
+//  jpexApp.swift
+//  jpex
+//
+//  Created by Terran Kroft on 26/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct jpexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
