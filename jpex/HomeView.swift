a5d6d5308a82717ff776f623dcb614f2397b7815
Terran <terrankroft@gmail.com>
2024-02-05 19:57:19 +1100

    bit of redesign

---
+++ jpex/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  jpex
+//
+//  Created by Terran Kroft on 5/2/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ jpex/PrefectureSave.swift
@@ -0,0 +1,8 @@
+//
+//  PrefectureSave.swift
+//  jpex
+//
+//  Created by Terran Kroft on 5/2/2024.
+//
+
+import Foundation
